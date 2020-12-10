51ad962db43611004ef6652abc594e1ec7faf5bc
coldea007 <48679994+coldea007@users.noreply.github.com>
2020-12-10 17:23:48 +0200

    Update Produse.h

--- Produse.h
+++ Produse.h
@@ -1,13 +1,16 @@
 #pragma once
-
+#include "Client.h"
 
 using namespace std;
 
-class Produse {
+class Produse : public Client {
 public:
 	Produse(char name, int cantitate, float pret);
+	Produse(float pret);
+	
 	Produse(const Produse &ob);
 	Produse &operator = (const Produse &ob);
+	Produse &operator+=(const Produse& ob2);
 	~Produse();
 	Produse &getProdusInstance();
 	char getName();
